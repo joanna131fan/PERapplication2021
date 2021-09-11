0feb0c4ddbdac4e873297b328183d5404bc22716
gittybot <42273839+joanna131fan@users.noreply.github.com>
2021-09-11 15:11:35 -0400

    Update TaskA.cpp

--- TaskA.cpp
+++ TaskA.cpp
@@ -8,6 +8,8 @@
 #include <thread>
 #include <cstdio>
 #include <cstdlib>
+#include <stdio.h>
+#include <typeinfo>
 
 
   using namespace cv;
@@ -17,16 +19,29 @@
 
   int main() {
       
-      Mat imgHSL, mask;
+      Mat imgHSL, mask, res;
       int hmin = 0, smin = 90, lmin = 102;
       int hmax = 18, smax = 250, lmax = 255;
       
-      string path = "/Users/joannafan/Desktop/C++/OpenCVCourse/OpenCVCourse/Resources/Task B/traffic-cones-5.ppm";
+      string path = "/Users/joannafan/Desktop/C++/OpenCVCourse/OpenCVCourse/Resources/Task A/traffic-cones-4.ppm";
+      const char* fileName = "/Users/joannafan/Desktop/C++/OpenCVCourse/OpenCVCourse/Resources/Task A/traffic-cones-4.ppm";
+      FILE* fr = fopen(fileName, "r");
+      int height, width, ccv;
+      char header[100];
+      fscanf(fr, "%s %d %d %d", header, &width, &height, &ccv);
+      
+//      cout << "Magic Number: " << header << endl;
+//      cout << "Width:" << width << endl;
+//      cout << "Height:" << height << endl;
+//      cout << "Maximum: " << ccv << endl;
+      if(strcmp(header, "P3") != 0 && strcmp(header, "P6") != 0)
+          return 0;
+      
       Mat img = imread(path);
 //      load_ppm(img, path);
       cvtColor(img, imgHSL, COLOR_BGR2HLS); // convert picture to HSL
       
-      /// For Testing Ranges of Hue, Saturation, and Luminance for all 4 test cases
+      /// For Testing Ranges of Hue, Saturation, and Luminance for test cases
 //      namedWindow("Trackbars");
 //      createTrackbar("Hue Min", "Trackbars", &hmin, 179);
 //      createTrackbar("Hue Max", "Trackbars", &hmax, 179);
@@ -36,15 +51,16 @@
 //      createTrackbar("Lum Max", "Trackbars", &lmax, 255);
       
       while(true) {
-      
+
           Scalar lower(hmin, smin, lmin);
           Scalar upper(hmax, smax, lmax);
           inRange(imgHSL, lower, upper, mask);
-          
+          bitwise_and(img, img, res, mask);
           imshow("Image", img);
+//          imshow("Image Mask", mask);
+          imshow("Image Final", res);
 //          imshow("Image HSL", imgHSL);
-          imshow("Image Mask", mask);
-          imwrite("/Users/joannafan/Desktop/C++/OpenCVCourse/OpenCVCourse/Resources/Task B/TC5.png", mask);
+          imwrite("/Users/joannafan/Desktop/C++/OpenCVCourse/OpenCVCourse/Resources/Task A/TC4.ppm", res);
           waitKey(5); // wait for 5 ms
 //          cout << "H: " << hmin << "," << hmax << endl;
 //          cout << "S: " << smin << "," << smax << endl;
